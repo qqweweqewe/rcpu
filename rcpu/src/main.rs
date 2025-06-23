46e3ee7651df8e18268546aafd932c7030b3fe08
qqweweqewe <golubkovivan4@gmail.com>
2025-06-23 18:12:56 +0300

    feat(api): working cpu endpoint
    - Implement CPU usage endpoint

--- rcpu/src/main.rs
+++ rcpu/src/main.rs
@@ -1,4 +1,5 @@
 mod info;
+mod error;
 
 use axum::{
     routing::get,
@@ -7,42 +8,38 @@ use axum::{
 };
 use serde::Serialize;
 
-// Define a response struct
+use crate::error::RcpuError;
+
 #[derive(Serialize)]
 struct Response {
-    message: String,
+    msg: String,
 }
 
 // GET "/cpu"
-async fn cpu() -> Json<Response> {
-    Json(Response {
-        message: "WIP".to_string(),
-    })
+async fn cpu() -> Result<Json<Response>, RcpuError> {
+    let load = info::cpu::get_load()?;
+    Ok(Json(Response {
+        msg: format!("{}", load),
+    }))
 }
 
 // GET "/ram"
 async fn ram() -> Json<Response> {
     Json(Response {
-        message: "WIP".to_string(),
+        msg: "WIP".to_string(),
     })
 }
 
 #[tokio::main]
 async fn main() {
-    // let app = Router::new()
-    //     .route("/cpu", get(cpu))
-    //     .route("/ram", get(ram));
+    let app = Router::new()
+        .route("/cpu", get(cpu))
+        .route("/ram", get(ram));
 
-    // let listener = tokio::net::TcpListener::bind("127.0.0.1:3000")
-    //     .await
-    //     .unwrap();
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000")
+        .await
+        .unwrap();
     
-    // println!("Server running on http://{}", listener.local_addr().unwrap());
-    // axum::serve(listener, app).await.unwrap();
-
-    for i in (0..5) {
-        println!("---------------");
-        let load = info::cpu::get_cpu_load().unwrap();
-        println!("LOAD: {:?}%", load)
-    }
+    println!("Server running on http://{}", listener.local_addr().unwrap());
+    axum::serve(listener, app).await.unwrap();
 }
