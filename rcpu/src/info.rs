b5b6d8c420b2a9645bb10988cc770ccb2dab9849
qqweweqewe <golubkovivan4@gmail.com>
2025-06-24 11:25:26 +0300

    feat(ram): implement ram mod
    - Implement RAM check functionality
    - Might extend later

--- rcpu/src/info.rs
+++ rcpu/src/info.rs
@@ -26,7 +26,7 @@ pub mod cpu {
         Ok((total, idle))
     }
 
-    pub fn get_load() -> Result<i32, RcpuError> {
+    pub fn get_load() -> Result<u8, RcpuError> {
         // get cpu stats at two moments of time
         let (prev_total, prev_idle) = get_data()?;
         thread::sleep(Duration::from_millis(100)); // poll interval
@@ -41,11 +41,50 @@ pub mod cpu {
 
         // compare idle time vs total work time - that basically is the load percentage
         // truncate unnecessary bytes, as percentage is never >100
-        let usage: i32 = (100 - idle_diff * 100 / total_diff) as i32;
+        let usage: u8 = (100 - idle_diff * 100 / total_diff) as u8;
         Ok(usage)
     }
 }
 
 pub mod ram {
+    use crate::error::RcpuError;
+    use std::fs;
 
+    const MEMINFO_PATH: &'static str = "/proc/meminfo";
+
+    fn get_data() -> Result<(u32, u32), RcpuError> {
+        // get the first two lines - MemTotal and MemFree
+        let mut values: Vec<String> = fs::read_to_string(MEMINFO_PATH)?
+            .lines()
+            .take(2)
+            .map(|s| s.to_string())
+            .collect();
+
+        // get the number itself
+        let free: u32 = values.pop().unwrap()
+            .split_whitespace()
+            .filter_map(|p| match p.parse::<u32>() {
+                Ok(val) => Some(val),
+                Err(_) => None
+            })
+            .collect::<Vec<u32>>()[1];
+
+        // get the number itself
+        let total: u32 = values.pop().unwrap()
+            .split_whitespace()
+            .filter_map(|p| match p.parse::<u32>() {
+                Ok(val) => Some(val),
+                Err(_) => None
+            })
+            .collect::<Vec<u32>>()[1];
+
+        Ok((free, total))
+    }
+
+    pub fn get_busy() -> Result<u8, RcpuError>{
+        let (free, total) = get_data()?;
+
+        // do I really need to explain this one?
+        Ok((100 - free*100/total) as u8)
+    }
 }
