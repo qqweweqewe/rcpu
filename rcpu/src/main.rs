33fa27d083875d8c83713eaf66fe0d2c4d670424
qqweweqewe <golubkovivan4@gmail.com>
2025-06-30 14:46:23 +0300

    refactor(api): simplify response, add disk endpoint
    - Add disk endpoint with a path parameter to fetch disk info
    - Simplify Response struct into simple json using serde

--- rcpu/src/main.rs
+++ rcpu/src/main.rs
@@ -2,40 +2,61 @@ mod info;
 mod error;
 
 use axum::{
-    routing::get,
-    Router,
-    Json,
+    extract::Path, routing::get, Json, Router
 };
-use serde::Serialize;
+use serde_json::{json, Value};
 
 use crate::error::RcpuError;
 
-#[derive(Serialize)]
-struct Response {
-    msg: String,
-}
-
 // GET "/cpu"
-async fn cpu() -> Result<Json<Response>, RcpuError> {
-    let load = info::cpu::get_load()?;
-    Ok(Json(Response {
-        msg: format!("{}", load),
-    }))
+async fn cpu() -> Result<Json<Value>, RcpuError> {
+    Ok(Json(json!(
+        {
+            "cpu": info::cpu::get_load()?
+        }
+    )))
 }
 
 // GET "/ram"
-async fn ram() -> Result<Json<Response>, RcpuError> {
-    let mem = info::ram::get_busy()?;
-    Ok(Json(Response {
-        msg: format!("{}", mem),
-    }))
+async fn ram() -> Result<Json<Value>, RcpuError> {
+    Ok(Json(json!(
+        {
+            "ram": info::ram::get_busy()?
+        }
+    )))
+
+}
+
+// Updated disk handler
+async fn disk(Path(format): Path<String>) -> Result<Json<Value>, RcpuError> {
+
+    match format.as_str() {
+        "percentage" => {
+            Ok(Json(json!(
+                { 
+                    "percentage": info::disk::percentage()? 
+                }
+            )))
+        }
+        "bytes" => {
+            let (total, used) = info::disk::bytes()?;
+            Ok(Json(json!(
+                { 
+                    "used": used, 
+                    "total": total 
+                }
+            )))
+        }
+        _ => Err(RcpuError::InvalidRequest),
+    }
 }
 
 #[tokio::main]
 async fn main() {
     let app = Router::new()
         .route("/cpu", get(cpu))
-        .route("/ram", get(ram));
+        .route("/ram", get(ram))
+        .route("/disk", get(disk));
 
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000")
         .await
